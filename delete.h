5f301e5f42e1c0e10b40a0a12a81541f325d2c6f
Maxioner <166314346+Maxioner@users.noreply.github.com>
2024-11-22 01:39:00 +0700

    Update delete.h

--- delete.h
+++ delete.h
@@ -1,9 +1,12 @@
 #pragma once
 #include <iostream>
-#include "parser.h"
-#include "tableWork.h"
-#include "rapidcsv.hpp"
+#include <fstream>
+#include "Node.h"
+#include "insert.hpp"
 
 using namespace std;
 
-void deleteRows(string command, JsonTable& jstab);
+bool ExistColonk(const string& tableName, const string& columnName, Node* TableHead);
+bool parseWhereClause(istringstream& iss2, string& table, string& column, string& value, const string& tableName, const TableJson& json_table);
+bool deleteRowsFromTable(const string& tableName, const string& column, const string& value, const TableJson& json_table);
+void delet(const string& command, const TableJson& json_table) ;
